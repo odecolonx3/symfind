617b65576b10187634c494c3bc8d44f71123e890
odecolon_x3 <odecolonx3@gmail.com>
2025-02-05 23:51:20 +0700

    Point symmetry along OX is working (kinda :)

--- symfind.py
+++ symfind.py
@@ -7,8 +7,9 @@ c = np.array([0.0, 0.0, 1.0])
 
 basis = np.array([[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]])
 
-d_phi = 0.001
-depth = 10
+epsilon = 0.001
+
+axis_order = [1, 2, 3, 4, 6]
 
 # print('symfind - simple crystal structure symmetry finder')
 
@@ -23,19 +24,19 @@ def rotate_x(A, phi):
     R_x = np.array([[1.0, 0.0,          0.0],
                     [0.0, np.cos(phi), -np.sin(phi)],
                     [0.0, np.sin(phi),  np.cos(phi)]])
-    return np.dot(R_x, A)
+    return np.transpose(np.dot(R_x, np.transpose(A)))
 
 def rotate_y(A, phi):
     R_x = np.array([[ np.cos(phi), 0.0, np.sin(phi)],
                     [ 0.0,         1.0, 0.0],
                     [-np.sin(phi), 0.0, np.cos(phi)]])
-    return np.dot(R_x, A)
+    return np.transpose(np.dot(R_x, np.transpose(A)))
 
 def rotate_z(A, phi):
     R_x = np.array([[np.cos(phi), -np.sin(phi), 0.0],
                     [np.sin(phi),  np.cos(phi), 0.0],
                     [0.0,          0.0,         np.cos(phi)]])
-    return np.dot(R_x, A)
+    return np.transpose(np.dot(R_x, np.transpose(A)))
 
 def to_cart(direct):
     return np.transpose(np.dot(np.transpose(np.array([a, b, c])), np.transpose(direct)))
@@ -56,10 +57,31 @@ def reduce(direct):
             v[2] += 1.0
     return direct
 
-for i in range(depth):
-    for j in range(depth):
-        for k in range(depth):
-            print('s')
+for o_x in axis_order:
+    # for o_y in axis_order:
+    #     for o_z in axis_order:
+            for i in range(o_x):
+                # for j in range(o_y):
+                #     for k in range(o_z):
+                        phi_x = 2 * np.pi / o_x * i
+                        # phi_y = 2 * np.pi / o_y * j
+                        # phi_z = 2 * np.pi / o_z * k
 
-print(to_cart(basis))
-print(to_direct(to_cart(basis)))
+                        if (i == 0) or ((o_x % i == 0) and (i != 1)):
+                            continue
+
+                        basis_ = to_cart(reduce(basis))
+                        basis_ = rotate_x(basis_, phi_x)
+                        # basis_ = rotate_y(basis_, phi_y)
+                        # basis_ = rotate_z(basis_, phi_z)
+                        basis_ = to_direct(basis_)
+                        basis_ = reduce(basis_)
+                        basis_ = to_cart(basis_)
+                        
+                        norm = 0.0
+
+                        for l in range(len(basis)):
+                            norm += np.sqrt((basis_[l][0] - to_cart(reduce(basis))[l][0])**2 + (basis_[l][1] - to_cart(reduce(basis))[l][1])**2 + (basis_[l][2] - to_cart(reduce(basis))[l][2])**2)
+
+                        if (norm < epsilon):
+                            print('symfind: point irreducible symmmetry rotation along OX found, phi = 2 pi / ', o_x, ' * ', i, ', order = ', o_x, ', with precision ~', epsilon)
